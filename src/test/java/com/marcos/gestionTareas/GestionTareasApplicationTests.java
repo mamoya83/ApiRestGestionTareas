826ffce3c35e6661e6ecff4a764233c4c5a8a18b
Marcos <mamoya@disid.com>
2025-05-19 10:33:36 +0200

    se agrega el proyecto

---
+++ src/test/java/com/marcos/gestionTareas/GestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcos.gestionTareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
